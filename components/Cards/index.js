b4b0b278805a2d621a23b9bf9cbbe79f6e8040d9
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-05 13:56:53 -0400

    mvp met: tabs and cards complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,62 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    Object.values(response.data.articles.bootstrap).forEach((element) => {
+      console.log(element);
+      cardMaker(element);
+    });
+    Object.values(response.data.articles.javascript).forEach((element) => {
+      //   console.log(element);
+      cardMaker(element);
+    });
+    Object.values(response.data.articles.jquery).forEach((element) => {
+      //   console.log(element);
+      cardMaker(element);
+    });
+    Object.values(response.data.articles.node).forEach((element) => {
+      //   console.log(element);
+      cardMaker(element);
+    });
+    Object.values(response.data.articles.technology).forEach((element) => {
+      //   console.log(element);
+      cardMaker(element);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  }, []);
+
+const cardContainer = document.querySelector(".cards-container");
+
+function cardMaker(dataInput) {
+  const cardDiv = document.createElement("div");
+  const cardHeadline = document.createElement("div");
+  const cardAuthorClass = document.createElement("div");
+  const cardAuthorName = document.createElement("span");
+  const cardContainerIMG = document.createElement("div");
+  const cardImage = document.createElement("img");
+
+  cardContainer.appendChild(cardDiv);
+  cardDiv.appendChild(cardHeadline);
+  cardDiv.appendChild(cardAuthorClass);
+  cardAuthorClass.appendChild(cardContainerIMG);
+  cardContainerIMG.appendChild(cardImage);
+  cardAuthorClass.appendChild(cardAuthorName);
+
+  cardDiv.classList.add("card");
+  cardHeadline.classList.add("headline");
+  cardAuthorClass.classList.add("author");
+  cardContainerIMG.classList.add("img-container");
+
+  cardAuthorName.textContent = dataInput.authorName;
+  cardHeadline.textContent = dataInput.headline;
+  cardImage.src = dataInput.authorPhoto;
+
+  return cardDiv;
+}
+
+console.log(cardMaker());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then((response) => {
+    console.log(response);
+    const lambdaData = response.data.topics;
+    console.log(lambdaData);
+
+    lambdaData.forEach((element) => {
+      tabMaker(element);
+    });
+  })
+  .catch((error) => {
+    console.log;
+  }, []);
+
+const topicContainer = document.querySelector(".topics");
+
+function tabMaker(tabContent) {
+  const tabDiv = document.createElement("div");
+
+  topicContainer.appendChild(tabDiv);
+
+  tabDiv.classList.add("tab");
+
+  tabDiv.textContent = tabContent;
+}
+
+console.log(tabMaker());
